a2f8a3c2aa698ba66326c6b7df0bd58275e71474
paul-111 <abarshapaul@gmail.com>
2024-06-10 01:57:49 +0600

    Add files

---
+++ src/pages/project.tsx
@@ -0,0 +1,31 @@
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Stack,Typography } from '@mui/material'
+import React from 'react'
+
+export const Project = () => {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+    <CardActionArea>
+      <CardMedia
+        component="img"
+        height="140"
+        image="/static/images/cards/contemplative-reptile.jpg"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+    <CardActions>
+      <Button size="small" color="primary">
+        Share
+      </Button>
+    </CardActions>
+  </Card>
+  )
+}
