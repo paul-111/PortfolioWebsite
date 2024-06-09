2de5989ac63f1022e4a01b18155bb8eadee137fe
unknown <barsha@swordfishtech.co.uk>
2024-06-09 23:47:36 +0600

    Contact Section

---
+++ src/constant/contact-constant.tsx
@@ -0,0 +1,12 @@
+const email =  require("../assets/img/contact/email.png");
+const github =  require("../assets/img/contact/git.png");
+const leetcode =  require("../assets/img/contact/code.png");
+const linkedin =  require("../assets/img/contact/linkedin.png");
+
+
+export const contact = [
+  { name: 'Email', icon: email },
+  { name: 'Gmail', icon: github },
+  { name: 'Leetcode', icon: leetcode },
+  { name: 'LinkedIn', icon: linkedin }
+];
