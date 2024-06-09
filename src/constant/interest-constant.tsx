d73ab11170ab2524028d6b92bd3ca1b9f31f2fdb
unknown <barsha@swordfishtech.co.uk>
2024-06-09 23:53:24 +0600

    Skill, Interest Section

---
+++ src/constant/interest-constant.tsx
@@ -0,0 +1,22 @@
+const book =  require("../assets/img/interest/book.png");
+const psychology =  require("../assets/img/interest/mental-health.png");
+const painting =  require("../assets/img/interest/mona-lisa.png");
+const music =  require("../assets/img/interest/music.png");
+const swimming =  require("../assets/img/interest/swimming.png");
+const travel =  require("../assets/img/interest/travel.png");
+const plants =  require("../assets/img/interest/plant.png");
+const badminton =  require("../assets/img/interest/bad.png");
+
+
+export const interests = [
+  { name: 'Books', icon: book },
+  { name: 'Psycology', icon: psychology },
+  { name: 'Art', icon: painting },
+  { name: 'Music', icon: music },
+  { name: 'Swimming', icon: swimming }, 
+  { name: 'Travel', icon: travel },
+  { name: 'Nature', icon: plants }, 
+  { name: 'Sports', icon: badminton },
+  { name: 'Photography', icon: plants }, 
+  { name: 'Art', icon: badminton }
+];
---
+++ src/pages/interest.tsx
@@ -0,0 +1,39 @@
+import { Grid, Typography, Box, Container, useTheme, useMediaQuery } from '@mui/material'
+import { interests } from '../constant/interest-constant';
+
+export const Interest = () => {
+  const theme = useTheme();
+  const isXSmallScreen = useMediaQuery(theme.breakpoints.down('xs'));
+  const isSmallScreen = useMediaQuery(theme.breakpoints.between('sm', 'md'));
+  const isMediumScreen = useMediaQuery(theme.breakpoints.between('md', 'lg'));
+  const isLargeScreen = useMediaQuery(theme.breakpoints.between('lg', 'xl'));
+  const isXLargeScreen = useMediaQuery(theme.breakpoints.up('xl'));
+  
+  return (
+    <Container sx={{ height: {sm: '100vh', md: '70vh'}, bgcolor: '#e9eef7' }} maxWidth={'xl'}>
+      <Typography sx={{ p:{ xs: 3, sm: 2, md: 1}, color: '#248a8a' }} variant='h6'> Interest </Typography>
+      <Grid container justifyContent="center" padding={1}>
+        {interests.map((skill, index) => (
+          <Grid item xs={5} sm={3} md={2} key={index}>
+            <Box sx={{ textAlign: 'center',  p: {xs: 2} }}>
+              <img
+                src={skill.icon}
+                alt={skill.name}
+                style={{
+                  width: '35px', 
+                  height: '35px', 
+                  marginTop: isXSmallScreen ? '10px' :
+                  isSmallScreen ? '15px' :
+                  isMediumScreen ? '20px' :
+                  isLargeScreen ? '30px' :
+                  isXLargeScreen ? '40px' : '0',
+                 }}
+              />
+              <Typography sx={{ pt: 1, color: 'grey' }} >{skill.name}</Typography>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
